b60d420d14b007872fa67a0fcc2f22343eaaffcb
wang <w344934197@163.com>
2021-10-21 01:17:35 +0800

    完成项目基础框架代码

---
+++ wisemed-commons/src/main/java/cn/xll/wisemed/base/controller/BaseController.java
@@ -0,0 +1,31 @@
+package cn.xll.wisemed.base.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * <b>系统基础信息类 - 控制层基础类</b>
+ * <p>
+ *     该类依赖注入了如下信息：</br>
+ *     1、HttpServletRequset</br>
+ *     2、HttpServletResponse</br>
+ *     3、HttpSession</br>
+ *     其余所有控制层都必须<b>继承于本控制层基础类</b>。
+ * </p>
+ * @author wang
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Controller("baseController")
+public class BaseController {
+	@Autowired
+	HttpServletRequest request;
+	@Autowired
+	HttpServletResponse response;
+	@Autowired
+	HttpSession session;
+}
